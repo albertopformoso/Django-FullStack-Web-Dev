b9a197b71636b785a083f29b134caa9ef2237b7b
albertopformoso <albertopformoso@gmail.com>
2021-03-13 18:41:45 -0600

    Document Object Model

---
+++ 01-DOM-Interaction/04-Game-Project/myscript.js
@@ -0,0 +1,31 @@
+// Restart Game Button
+var restart = document.querySelector('#restart');
+
+// Grab all the squares
+var squares = document.querySelectorAll("td");
+
+
+// Clear Squares Function
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].textContent = '';
+  }
+
+}
+restart.addEventListener('click', clearBoard)
+
+
+function changeMarker(){
+    if (this.textContent === '') {
+      this.textContent = 'X';
+    } else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    } else {
+      this.textContent = '';
+    }
+};
+
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+}
